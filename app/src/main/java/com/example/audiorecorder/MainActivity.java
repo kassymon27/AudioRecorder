94bc395f5e28130dcb0fab6ab39087d653a80dec
kassym <abilkasimov2000@gmail.com>
2020-07-15 23:34:37 +0600

    Added Basic variables and some addtitional code in function 'startRecording'

--- app/src/main/java/com/example/audiorecorder/MainActivity.java
+++ app/src/main/java/com/example/audiorecorder/MainActivity.java
@@ -1,20 +1,38 @@
 package com.example.audiorecorder;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private String [] permissions = {Manifest.permission.RECORD_AUDIO};
+    private int REQUEST_RECORD_AUDIO_PERMISSION = 200;
+    private String filename = null;
+    MediaRecorder mediaRecorder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
+
+
+
+
     }
 
+
     public void startRecording(View view){
+        mediaRecorder = new MediaRecorder();
+        mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_2_TS);
+        mediaRecorder.setOutputFile(filename);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
 
     }
 }
