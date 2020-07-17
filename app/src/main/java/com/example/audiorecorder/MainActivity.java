efe5f3188c4538faeb3d86af3136d36331176edf
kassym <abilkasimov2000@gmail.com>
2020-07-17 20:41:23 +0600

    Added code in stopButton that will stop recording

--- app/src/main/java/com/example/audiorecorder/MainActivity.java
+++ app/src/main/java/com/example/audiorecorder/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import java.io.IOException;
@@ -13,35 +14,44 @@ import java.io.IOException;
 public class MainActivity extends AppCompatActivity {
     private String [] permissions = {Manifest.permission.RECORD_AUDIO};
     private int REQUEST_RECORD_AUDIO_PERMISSION = 200;
-    private String filename = null;
+    private String fileName = null;
     MediaRecorder mediaRecorder;
+    private static final String LOG_TAG = "AudioRecord";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
+        fileName = getExternalCacheDir().getAbsolutePath();
+        fileName += "/audiorecordtest.3gp";
 
-
-
-
+      //  ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
     }
 
 
-    public void startRecording(View view) throws IOException {
+    public void startRecording(View view)  {
         mediaRecorder = new MediaRecorder();
         mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_2_TS);
-        mediaRecorder.setOutputFile(filename);
-        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-        mediaRecorder.prepare();
+        mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        mediaRecorder.setOutputFile(fileName);
+        mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
+        try {
+            mediaRecorder.prepare();
+        } catch (IOException e) {
+            Log.e(LOG_TAG, "prepare() failed");
+
+        }
+
         mediaRecorder.start();
 
 
     }
 
     public void stopButton(View view){
+        mediaRecorder.stop();
+        mediaRecorder.release();
 
     }
 
